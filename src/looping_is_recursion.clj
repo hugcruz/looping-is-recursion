3b242bde8fedc04a81649d58f306a0a5749e3915
Hugo Cruz <hugomfcruz@gmail.com>
2017-07-25 21:55:28 +0100

    All tests passing

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -45,12 +45,47 @@
       (recur (+ acc (first my-seq)) (rest my-seq))
 )))
 
+(defn toggle [a-set elem]
+  (if (contains? a-set elem)
+   (disj a-set elem)
+   (conj a-set elem)
+))
+
 (defn parity [a-seq]
-  ":(")
+  (loop [
+          results #{}
+          my-seq  a-seq
+        ]
+    (if (empty? my-seq)
+      results
+      (recur (toggle results (first my-seq)) (rest my-seq))
+)))
+
 
 (defn fast-fibo [n]
-  ":(")
+  (loop [
+          cnt 2
+          n1  1
+          n2  0
+        ]
+    (cond
+      (= n   0) 0
+      (= n   1) 1
+      (= cnt n) (+ n1 n2)
+      :else     (recur (inc cnt) (+ n1 n2) n1)
+)))
 
 (defn cut-at-repetition [a-seq]
-  [":("])
+  (loop [
+          results []
+          my-seq  a-seq
+        ]
+    (cond
+      (empty? my-seq)
+        results
+      (some #(= (first my-seq) %) results)
+        (recur results (rest my-seq))
+      :else
+        (recur (conj results (first my-seq)) (rest my-seq))
+)))
 
